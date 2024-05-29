a15fccc7ed58c40345d755fc263b372e2fdc8fec
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-05-28 22:41:09 -0300

    Add files via upload

--- erp/erp/src/main/java/br/unipar/erp/data/entity/Usuario.java
+++ erp/erp/src/main/java/br/unipar/erp/data/entity/Usuario.java
@@ -1,17 +1,31 @@
 package br.unipar.erp.data.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
+
 @Entity
+@Getter
+@Setter
+//@Data faz a mesma coisa do Getter e Setter, deixa mais clean
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "usuarios")
 public class Usuario {
     @Id
     private Integer codigo;
-
-    private Integer usuario;
-
-    private Integer senha;
-
+    @Column(name = "username")
+    private String usuario;
+    @Column(name = "password")
+    private String senha;
+    @Column(name = "nascimento")
+    private String nome;
     private LocalDate dataNascimento;
 }
